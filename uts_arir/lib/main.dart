ab06ba31892bf97c108521afb60b792ba557c670
ArirurRohman <arirfadili@gmail.com>
2026-03-24 01:44:33 +0700

    tambahan 2

--- uts_arir/lib/main.dart
+++ uts_arir/lib/main.dart
@@ -18,3 +18,40 @@ class MyBCAApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF005DAA), // Warna biru BCA
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                // Header Section
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'myBCA',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      Row(
+                        children: const [
+                          Icon(Icons.settings, color: Colors.white),
+                          SizedBox(width: 15),
+                          Icon(Icons.logout, color: Colors.white),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
