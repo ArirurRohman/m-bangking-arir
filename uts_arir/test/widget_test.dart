c3a73bd0b7d44b2df617f29dbd8115a07bf404a6
ArirurRohman <arirfadili@gmail.com>
2026-03-25 00:11:02 +0700

    finishing

--- uts_arir/test/widget_test.dart
+++ uts_arir/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:uts_arir/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyBCAApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
