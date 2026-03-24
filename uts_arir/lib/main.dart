7d566f5273c399b8d900abd8f5e7ab6819813256
ArirurRohman <arirfadili@gmail.com>
2026-03-24 19:47:25 +0700

    penambahan 3

--- uts_arir/lib/main.dart
+++ uts_arir/lib/main.dart
@@ -55,3 +55,33 @@ class HomePage extends StatelessWidget {
                     ],
                   ),
                 ),
+                   // Content White Area
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                    child: SingleChildScrollView(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text("HALO, ANDHINI PUTRI", 
+                            style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFF005DAA))),
+                          const SizedBox(height: 15),
+                          
+                          // Saldo Card
+                          Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(color: Colors.grey.withOpacity(0.2), blurRadius: 10, spreadRadius: 2)
+                              ],
+                            ),
